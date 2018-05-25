241946c4c7408374a03debbb16bdff42b45a96fc
Patrick Butler <lafd14@gmail.com>
2018-05-25 15:03:34 -0700

    start website

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const AboutPage = () => (
+  <div>
+<h1>About Us</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam modi dolore quam unde voluptates doloribus nisi ut quis eaque aliquid repellat, rerum corrupti explicabo, optio laborum! Sit debitis corrupti repellat.</p>
+  <Link to="/">Home</Link>
+  </div>
+)
+
+export default AboutPage
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import './index.css'
+const IndexPage = () => (
+  <div>
+    <h1>Fire-Test</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/about/">Go to About</Link>
+  </div>
+)
+
+export default IndexPage
